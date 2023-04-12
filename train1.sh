42021a404c3da8d2098159a9d691e13a2e9e1807
KimGyunYeop <gyop0817@naver.com>
2023-04-12 15:40:40 +0900

    add folder

--- train1.sh
+++ train1.sh
@@ -1,18 +1,18 @@
 #/bin/bash
 
-PREV_TURN=(0 2)
-CA_EMB_NUM=(0 1 10 100)
-GPU=1
+PREV_TURN=(2)
+CA_EMB_NUM=(10)
+GPU=2
 
 for prev_turn in ${PREV_TURN[@]}; do
     for ca_emb_num in ${CA_EMB_NUM[@]}; do
-
         python -u main.py\
-            --result_path "t5_CA_prev_{$prev_turn}_embnum_{$ca_emb_num}" \
+            --result_path "T5AEModel_prev_{$prev_turn}_embnum_{$ca_emb_num}_prevloss_1" \
             --prev_turn $prev_turn \
-            --T5CAModel \
-            --CAEmbedding_num $ca_emb_num \
+            --prev_turn_loss \
+            --d 1 \
+            --T5AEModel \
+            --AEEmbedding_num $ca_emb_num \
             --gpu $GPU
-
-    done
+    done 
 done
